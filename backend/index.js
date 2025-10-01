1204fcdb82c6c23ae114e384f0f44160c60d5bc8
Rahul-mahato963 <rahulmahaseth146@gmail.com>
2025-10-02 00:10:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
+
 mongoose.connect("mongodb://127.0.0.1:27017/todoDB")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-// Routes
+
 app.use("/api/tasks", taskRoutes);
 
 const PORT = 5000;
